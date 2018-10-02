a63d5f252ce389ef3ddbc187ff45fb050d63b36c
Charles Baafi <baaficharles036@gmail.com>
2018-10-02 23:31:12 +0000

    first comand

--- 00_hello/hello.rb
+++ 00_hello/hello.rb
@@ -1,1 +1,15 @@
-#write your code here
+def hello(name)
+    if name == "Charles"
+    puts "Hello #{name}"
+    else name == "Yaw"
+        puts "Hello World"
+    end
+end
+
+def main
+     prints "What is your name"
+     name= gets.chomp
+     hello(name)
+end
+
+main if __FILE__ == $PROGRAM_NAME
